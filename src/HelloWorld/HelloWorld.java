af49b615ba3e8e7e526d0288b216e25e7a98ff4e
QuyV8 <Quy.vu0992@gmail.com>
2025-01-26 15:38:16 -0800

    Added HelloWorld

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,7 +1,6 @@
-package HelloWorld;
-
 public class HelloWorld {
     public static void main(String[] args) {
+        // Output the classic "Hello, World!" message
         System.out.println("Hello, World!");
     }
 }
